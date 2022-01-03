385d0d0962dd5ebcfbe71a3639103288d35df82e
Collin Stevens <qollin@protonmail.com>
2022-01-03 05:57:45 -0700

    fixup script and entrypoint

--- handmade/code/win32_handmade.cpp
+++ handmade/code/win32_handmade.cpp
@@ -1,28 +1,12 @@
 #include <windows.h>
 
-#include <direct.h> // _getcwd
-#include <stdlib.h> // free, perror
-#include <stdio.h>  // printf
-#include <string.h> // strlen
-
 int CALLBACK
 WinMain(HINSTANCE hInstance,
 		HINSTANCE hPrevInstance,
 		LPSTR     lpCmdLine,
 		int       nShowCmd)
 {
-	MessageBox(0, "This is Handmade Hero.", "Handmade Hero", MB_OK | MB_ICONINFORMATION);
-
-	char* buffer;
-
-	// Get the current working directory:
-	if ((buffer = _getcwd(NULL, 0)) == NULL)
-		perror("_getcwd error");
-	else
-	{
-		printf("%s \nLength: %zu\n", buffer, strlen(buffer));
-		free(buffer);
-	}
+	MessageBoxA(0, "This is Handmade Hero.", "Handmade Hero", MB_OK | MB_ICONINFORMATION);
 
 	return(0);
 }
